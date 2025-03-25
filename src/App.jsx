200670e61e2edb85d53472885fa46bde0e9134a1
Abhishekr2122 <abhishekrshinde21@gmail.com>
2025-03-25 17:39:33 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="text-3xl font-bold underline">
+        Welcome to the Apple website project
+      </p>
+    </>
+  );
+}
+
+export default App;
