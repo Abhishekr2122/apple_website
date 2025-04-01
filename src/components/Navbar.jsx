bdb97914ff7992246b7ec97637994462e5dd48b7
Abhishekr2122 <abhishekrshinde21@gmail.com>
2025-04-01 15:08:03 +0530

    Done building Navbar element

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,29 @@
+import { appleImg, bagImg, searchImg } from "../utils";
+
 export default function Navbar() {
-  return <div>This is the navbar section</div>;
+  const navItems = ["Store", "Mac", "iPhone", "Support"];
+  return (
+    <header className="w-full     items-center    py-5 sm:px-10 px-5 flex   ">
+      <nav className="flex w-full  screen-max-width  justify-between ">
+        <img src={appleImg} alt="Apple" width={14} height={18} />
+        <div className="flex  flex-1 justify-center max-sm:hidden ">
+          {navItems.map(function (citem) {
+            return (
+              <div
+                key={citem}
+                className="px-5 text-sm text-gray cursor-pointer hover:text-white transition-all"
+              >
+                {citem}
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="flex items-baseline gap-7 max-sm:justify-end  max-sm:flex-1">
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="bag" width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  );
 }
